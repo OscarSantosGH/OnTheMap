47cb9f12c7b3ed5958dfa4b85a88efb5402f4563
Oscar Santos <oscarsantos.go@gmail.com>
2020-06-16 23:26:46 -0400

    added: StudentResponse

---
+++ OnTheMap/OTM Responses/StudentsResponse.swift
@@ -0,0 +1,13 @@
+//
+//  StudentsResponse.swift
+//  OnTheMap
+//
+//  Created by Oscar Santos on 6/16/20.
+//  Copyright © 2020 Oscar Santos. All rights reserved.
+//
+
+import Foundation
+
+struct StudentResponse: Codable {
+    let results:[StudentInformation]
+}
