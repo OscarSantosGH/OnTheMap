d52307e03368048f9e7f155bc3dc0668c46b2896
Oscar Santos <oscarsantos.go@gmail.com>
2020-07-07 22:14:25 -0400

    added: PostStudentLocationResponse struct

---
+++ OnTheMap/OTM Responses/PostStudentLocationResponse.swift
@@ -0,0 +1,14 @@
+//
+//  PostStudentLocationResponse.swift
+//  OnTheMap
+//
+//  Created by Oscar Santos on 7/7/20.
+//  Copyright © 2020 Oscar Santos. All rights reserved.
+//
+
+import Foundation
+
+struct PostStudentLocationResponse: Codable {
+    let createdAt:String
+    let objectId:String
+}
